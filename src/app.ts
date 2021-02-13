ab872fc25a3c04752a103303f7f0b2f5514fb8c2
Positive-Conative <kyj970513@naver.com>
2021-02-13 18:42:56 +0900

    Git Test

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,21 @@
-import express, {Request, Response} from 'express'
+import express from 'express'
 import { createConnection } from 'typeorm';
-import {boardRepo} from "./repository/boardRepo";
-import {Board} from "./entity/Board";
+import bodyParser from 'body-parser';
 
 const app = express()
+
+//body-parser 설정
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true,}));
+
 //DB connect
 createConnection()
-  .then(async connection => {
-    interface Error {//err handler 추가해야함..!
+.then(async connection => {
+    interface Error {   //err handler 추가해야함..!
       status?: number;
       message?: string;
     }
-    // const userRepository = connection.getRepository(Board);
-    // const user = new Board();
-    // user.firstName = "Timber";
-    // user.lastName = "Saw";
-    // user.isActive = true;
-    // await userRepository.save(user);
-
-
-    // const timber = await boardRepo(1, "Timber");
-    // const userRepository : any = connection.getCustomRepository(boardRepo);
-    // const timber = await userRepository.findByName("Timber", "Saw");
-    // console.log(timber)
-})
-.catch(err=>console.log(err))
+}).catch(err=>console.log(err))
 
 import indexRouter from "./router/index";
 app.use("/", indexRouter);
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,16 @@
 import express, {Request, Response, NextFunction} from 'express'
 import "reflect-metadata";
-import {createConnection} from "typeorm";
 import {User} from "../entity/User";
 import {Board} from "../entity/Board";
-import {boardRepo} from "../repository/boardRepo";
+import {getConnection} from "typeorm";
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req:Request, res:Response, next)=>{
-    /*
-    var hello=createConnection().then(async connection => {
-        console.log("Inserting a new user into the database...");
-        const user = new User();
-        user.pw = "Timber";
-        await connection.manager.save(user);
-        const users = await connection.manager.find(User);
-        console.log("Loaded users: ", users);
-        return users
-        // await connection.close();
-        // console.log("Here you can setup and run express/koa/any other framework.");
-    }).catch(error => console.log(error));
-
-    res.send(hello)
-*/
+indexRouter.post('/', (req:Request, res:Response, next)=>{
+    console.log(req.body.id)
+    var hello : string = req.body.id
+    res.json({"id":hello})
 })
-import {getConnection} from "typeorm";
 
 indexRouter.get('/active_record', async (req:Request, res:Response, next)=>{
     const timber = await User.findByName(1, "Timber");
