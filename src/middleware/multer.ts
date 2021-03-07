3cd1f16a99db9b63a8ce0f38b9d460e7cf46def0
Positive-Conative <kyj970513@naver.com>
2021-03-07 07:53:37 +0000

    Edit : Multer(divide file & image)

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,14 +1,22 @@
-// 공지사항 등록
-// MULTER 설정
-
+import { logger } from "../config/logger";
 var multer = require('multer');
+
 var storage = multer.diskStorage({
     destination: function (req : Request, file : any, cb : any) {
-      cb(null, 'src/public/upload/') 
+      if(file.fieldname == 'image') cb(null, 'src/public/upload/images')
+      else if(file.fieldname == 'file') cb(null, 'src/public/upload/files')
+      else{
+        logger.error({
+            label:"[Multer.ts - ETC]",
+            message: `\n\t└ err : `+ "fieldname이 image도 아니며, file도 아닙니다."
+        })
+      }
     },
     filename: function (req : Request, file : any, cb : any) {
       cb(null, file.originalname) 
     }
   })
+
 var upload = multer({ storage: storage });
-  module.exports = upload;
+
+module.exports = upload;
